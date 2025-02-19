e969dde19f755ba9a34a3e0fe78bc3836fb7732b
thghost21 <tylerhacker21@gmail.com>
2025-02-19 14:08:01 -0700

    layout rough draft

---
+++ app.js
@@ -0,0 +1,31 @@
+let bank = 100
+
+const players = [
+  { teamNumber: 1, emoji: '🏃‍♂️', skill: 10, name: "D'Marcus Williums" },
+  { teamNumber: 1, emoji: '🤾‍♂️', skill: 30, name: "Tyroil Smoochie-Wallace" },
+  { teamNumber: 1, emoji: '🏇', skill: 88, name: "Jackmerius Tacktheratrix" },
+  { teamNumber: 1, emoji: '🏌️‍♀️', skill: 15, name: "Javaris Jamar Javarison-Lamar" },
+  { teamNumber: 1, emoji: '🏋️‍♂️', skill: 77, name: "D'Pez Poopsie" },
+  { teamNumber: 1, emoji: '🏌️‍♂️', skill: 21, name: "D'Jasper Probincrux III" },
+  { teamNumber: 1, emoji: '🤾', skill: 5, name: "Leoz Maxwell Jilliumz" },
+  { teamNumber: 1, emoji: '🏂', skill: 99, name: "Hingle McCringleberry" },
+  { teamNumber: 1, emoji: '🧘‍♀️', skill: 50, name: "L'Carpetron Dookmarriot" },
+  { teamNumber: 1, emoji: '🚶‍♀️', skill: 1, name: "Xmus Jaxon Flaxon-Waxon" },
+  { teamNumber: 2, emoji: '🏋️‍♀️', skill: 61, name: "Saggitariutt Jefferspin" },
+  { teamNumber: 2, emoji: '🤺', skill: 34, name: "Quatro Quatro" },
+  { teamNumber: 2, emoji: '🏄', skill: 71, name: "X-Wing @Aliciousness" },
+  { teamNumber: 2, emoji: '🧜‍♂️', skill: 76, name: "Bisquiteen Trisket" },
+  { teamNumber: 2, emoji: '🤸', skill: 47, name: "Scoish Velociraptor Maloish" },
+  { teamNumber: 2, emoji: '⛹️‍♀️', skill: 23, name: "Donkey Teeth" },
+  { teamNumber: 2, emoji: '🕴️', skill: 58, name: "T.J. A.J. R.J. Backslashinfourth V" },
+  { teamNumber: 2, emoji: '💃', skill: 99, name: "Firstname Lastname" },
+  { teamNumber: 2, emoji: '🧍‍♂️', skill: 3, name: "Dan Smith" },
+  { teamNumber: 2, emoji: '🐅', skill: 100, name: "Tiger" },
+]
+
+
+function drawTeam1() {
+  const team1Container = document.getElementById('team1'); // Grab Where, Team 1 will draw
+  team1Content = ''; // Create empty  container first
+  //.. loop through players adding only those on team1
+}
